a1f0bee652ff5fecb70b87638879513f13661b1e
Joaop <Joaop@SLOWPC>
2023-10-17 11:51:43 +0100

    Add project files.

---
+++ MadeiraDeCedroWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadeiraDeCedroWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
