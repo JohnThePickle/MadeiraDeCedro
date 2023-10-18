5cfabac7d0561fea5d0ec3d3de67783217425fb3
Joaop <Joaop@SLOWPC>
2023-10-18 15:25:24 +0100

    18-10-2023 Criação do projeto Criação da class Serviços Nuget packages Criação da base de dados Substituição do bootstrap e do Jquery

---
+++ MadeiraDeCedro.Domain/Entities/Servicos.cs
@@ -0,0 +1,15 @@
+﻿namespace MadeiraDeCedro.Domain.Entities
+{
+    public class Servicos
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; } //Nome do serviço
+        public string? Descrição{ get; set;} //Descrição do serviço
+        public string? Madeira { get; set; } //Tipo de Madeira
+        public string? Movel { get; set; } //Tipo de Movel
+        public string? Imagem { get; set; } //ImageUrl
+        public DateTime? DataPedido { get; set; }
+        public DateTime? DataCriacao { get; set; } //Data da criação do Movel
+
+    }
+}
