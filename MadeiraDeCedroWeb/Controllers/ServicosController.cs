18ee56a3a7f833c94ad83f46cbfaa610fd7e764b
Joaop <Joaop@SLOWPC>
2023-11-15 21:18:04 +0000

    -Adicionar os botões de atualizar e apagar campos da tabela. -Adicionar a vista Update, que atualiza os campos da tabela na base de dados.

--- MadeiraDeCedroWeb/Controllers/ServicosController.cs
+++ MadeiraDeCedroWeb/Controllers/ServicosController.cs
@@ -22,6 +22,37 @@ namespace MadeiraDeCedroWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Servicos obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Servicos.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+            public ActionResult Update(int ServicoId)
+            {
+                Servicos? obj = _db.Servicos.FirstOrDefault(u => u.Id == ServicoId);
 
+                if (obj == null)
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+                return View(obj);
+            }
+            [HttpPost]
+            public IActionResult Update(Servicos obj)
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Servicos.Update(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(obj);
+            }
     }
 }
---
+++ dbo.Servicos.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Servicos] ON
+INSERT INTO [dbo].[Servicos] ([Id], [Nome], [Descrição], [Madeira], [Movel], [Imagem]) VALUES (1, 2, 3, 4, 5, 6);
+SET IDENTITY_INSERT [dbo].[Servicos] OFF
