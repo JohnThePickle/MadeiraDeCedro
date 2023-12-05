b0eb3f278ad81918fb57cbc6cd3a75d33040be80
Joaop <Joaop@SLOWPC>
2023-12-05 23:16:09 +0000

    Atualização da pagina inicial. Adição do logotipo, imagem de banner e texto na pagina principal Atualização do footer adicionando simbolos de bootstrap bem como estilos com gradientes e cores solidas Implementação do embed de posts do instagram e desenvolvimento de funcionalidade tipo carrossel.

--- MadeiraDeCedroWeb/wwwroot/js/site.js
+++ MadeiraDeCedroWeb/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+const dots = document.querySelectorAll('.dot');
 
-// Write your JavaScript code.
+// Set up event listeners for buttons
+document.querySelector('.prev').addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateCarousel();
+});
+
+// Set up event listeners for dots
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentSlide = index;
+        updateCarousel();
+    });
+});
+
+function updateCarousel() {
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
+    });
+
+    // Update dots
+    dots.forEach((dot, index) => {
+        if (index === currentSlide) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+}
+
+// Initialize the carousel
+updateCarousel();
